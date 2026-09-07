936f199e97845904ff5502c8a2f65b42fb53d730
Piyush <davidpiyush225@gmail.com>
2026-09-07 13:29:03 +0530

    add footer

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import About from "./_component/About"
+import Footer from "./_component/Footer"
 import Header from "./_component/Header"
 
 function page() {
   return (
     <div>
       <Header/>
+      <About/>
+      <Footer/>
     </div>
   )
 }
