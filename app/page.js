881851acd2c7560bef4f19c5cd807fcb6a023796
li-alzz02 <alishakumari642@gmail.com>
2026-09-07 12:18:32 +0530

    hero and contact page by alisha

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
+import Contact from "./_component/Contact"
+import Dashboard from "./_component/Dashboard"
 import Header from "./_component/Header"
+import Hero from "./_component/Hero"
 
 function page() {
   return (
     <div>
       <Header/>
+      <Hero/>
+      <Contact/>
+      {/* <Dashboard/> */}
     </div>
   )
 }
